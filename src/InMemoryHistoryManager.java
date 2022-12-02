8c58edb7720a3b9fd534ea60f93ff070e7763bf5
Александр Самсонов <samsonov110294@yandex.ru>
2022-12-02 19:55:17 +0300

    refactor: добавить константу MAX_LENGTH_LIST refactor: измеить history с класса ArrayList на Linkedlist

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-
-    private static List<Task> history = new ArrayList<>();
+    public final int MAX_LENGTH_LIST = 10;
+    private static LinkedList<Task> history = new LinkedList<>();
 
     @Override
     public void add(Task task) {
-        if (history.size() < 10) {
-            history.add(task);
+        if (history.size() < MAX_LENGTH_LIST) {
+            history.addLast(task);
         } else {
-            history.remove(0);
-            history.add(task);
+            history.pollFirst();
+            history.addLast(task);
         }
     }
 
